1a120ee49806fbe9011c6c524e6c09106512cc66
wali.rahman89@yahoo.com <wali.rahman89@yahoo.com>
2017-11-16 12:53:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ function BoardMember(name, homestate, training) {
   this.homestate = homestate;
   this.training = training;
   this.veto = function veto() {
-    returns "No, I must disagree";
+    return "No, I must disagree";
+  }
+  this.approve = function approve() {
+    return "You can do that!";
+  }
+  this.doCharity = function charity() {
+    return "I like to help people.";
+  }
+  this.releasePressStatement = function releasePressStatement() {
+    return "You will see great things from Scuber";
+  }
+  this.sayHi = function sayHi() {
+    return "Hi, my name is " + this.name + ". I am from " + this.homestate + ", and I was trained in " + this.training + "."; 
   }
 }
